8c33d341fcd211cd7782bfc63c2df1acf9dfb463
maiona <maiona@gmail.com>
2015-08-30 20:46:19 -0300

    Picture taking and Saving func. implemented
    Picture taking API using PhoneGap + Firebase to store and get user's
    pics.

--- js/app.js
+++ js/app.js
@@ -36,6 +36,9 @@ photocloud.run(function($ionicPlatform) {
 .controller('AuthCtrl', ['$scope', '$state', '$firebaseAuth', function($scope, $state, $firebaseAuth){
   auth = $firebaseAuth(firebase);
 
+  //Get Login Status
+  firebase.onAuth(getStatusCallback);
+
   $scope.login = function(username, password){
     auth.$authWithPassword({
       email: username,
@@ -59,8 +62,72 @@ photocloud.run(function($ionicPlatform) {
       console.log('Error: '+ error)
     });
   }
+
+  $scope.logout = function() {
+    console.log('Logging out');
+    auth.$unauth();
+    $state.go('login');
+  }
+
+  function getStatusCallback(authData){
+    if (authData) {
+      console.log('User ' + authData.uid + ' is logged in with ' + authData.provider);
+    } else {
+      console.log('No user logged in.');
+    }
+
+  }
 }])
 
-.controller('PhotosCtrl', ['$scope', '$state', '$firebaseAuth', function($scope, $state, $firebaseAuth){
-  console.log('PhotosCtrl Loaded');
+.controller('PhotosCtrl', ['$scope', '$state', '$ionicHistory', '$cordovaCamera', '$firebaseArray', function($scope, $state, $ionicHistory, $cordovaCamera, $firebaseArray){
+  $ionicHistory.clearHistory();
+
+  $scope.images = [];
+
+  var auth = firebase.getAuth();
+
+  //Get Login Status
+  firebase.onAuth(getStatusCallback);
+
+  if (auth) {
+    var userReference = firebase.child('users/' + auth.uid);
+    var syncArray = $firebaseArray(userReference.child('images'));
+    $scope.images = syncArray;
+  } else {
+     $state.go('login');
+  }
+
+  $scope.upload = function() {
+    var options = {
+      quality: 75,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      popoverOptions: CameraPopoverOptions,
+      targetWidth: 500,
+      targetHeight: 500,
+      saveToPhotoAlbum: false
+    };
+
+    //GET PICTURE
+    $cordovaCamera.getPicture(options).then(function(data){
+      //SAVE TO DATABASE
+      syncArray.$add({image: data}).then(function(){
+        alert('Image uploaded!');
+      })
+    }, function(error){
+      console.log('Error: ' + error);
+      alert('Error: ' + error);
+    });
+  }
+
+  function getStatusCallback(authData){
+    if (authData) {
+      console.log('User ' + authData.uid + ' is logged in with ' + authData.provider);
+    } else {
+      console.log('No user logged in.');
+    }
+
+  }
 }])
